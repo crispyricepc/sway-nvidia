81c287531c9f5094971ad4397dee8ab1f56af7b6
Ben <bjosephmitchell@gmail.com>
2022-04-01 19:54:41 +0100

    Source shell things

--- sway-nvidia.sh
+++ sway-nvidia.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env sh
 
+# Source profile (lifted from some gnome stuff)
+if [ "x$XDG_SESSION_TYPE" = "xwayland" ] &&
+   [ "x$XDG_SESSION_CLASS" != "xgreeter" ] &&
+   [  -n "$SHELL" ] &&
+   grep -q "$SHELL" /etc/shells &&
+   ! (echo "$SHELL" | grep -q "false") &&
+   ! (echo "$SHELL" | grep -q "nologin"); then
+  if [ "$1" != '-l' ]; then
+    exec bash -c "exec -l '$SHELL' -c '$0 -l $*'"
+  else
+    shift
+  fi
+fi
+
 # Source environment files
 for n in "/usr/share" "/usr/local/share"; do
   [ -f "$n/wlroots-nvidia/wlroots-env-nvidia.sh" ] && . "$n/wlroots-nvidia/wlroots-env-nvidia.sh"
   [ -f "$n/wlroots-nvidia/wlroots-env.sh" ] && . "$n/wlroots-nvidia/wlroots-env.sh"
 done
 
-exec sway --unsupported-gpu
+exec sway --unsupported-gpu "$@"
