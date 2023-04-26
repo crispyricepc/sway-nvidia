409ea0973d639037d9b0f0327a0531fd04262d3f
weebney <weebney@gmail.com>
2023-04-26 12:10:36 -0400

    Add XWAYLAND_NO_GLAMOR=1 to wlroots env vars

--- wlroots-env-nvidia.sh
+++ wlroots-env-nvidia.sh
@@ -4,7 +4,7 @@
 
 # Hardware cursors not yet working on wlroots
 export WLR_NO_HARDWARE_CURSORS=1
-# Set WLRoots renderer to Vulkan to avoid flickering
+# Set wlroots renderer to Vulkan to avoid flickering
 export WLR_RENDERER=vulkan
 # General wayland environment variables
 export XDG_SESSION_TYPE=wayland
@@ -18,3 +18,5 @@ export GBM_BACKEND=nvidia-drm
 export __GL_GSYNC_ALLOWED=0
 export __GL_VRR_ALLOWED=0
 export __GLX_VENDOR_LIBRARY_NAME=nvidia
+# Xwayland compatibility
+export XWAYLAND_NO_GLAMOR=1
